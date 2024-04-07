cc27a1c520d5cc0c4fe650815d416716baea3ba3
Sur1020 <sur.stepanyan.20@gmail.com>
2024-04-07 17:11:15 +0400

    lesson_7

--- lesson_7/task7.py
+++ lesson_7/task7.py
@@ -10,35 +10,6 @@ database = database
 
 cursor = connection.cursor()
 
-# query = '''
-#     SELECT 
-#         users.id,
-#         users.first_name,
-#         users.last_name,
-#         users.email,
-#         users.age,
-#         users.country,
-#         user_details.height,
-#         user_details.weight,
-#         user_details.eye_color,
-#         user_finance.salary,
-#         user_finance.bonus,
-#         user_mobiles.phone_number,
-#         user_mobiles.operator,
-#         user_mobiles.mobile_country,
-#         cars.car,
-#         cars.car_number,
-#         cars.model,
-#         cars.year
-#     FROM users
-#     LEFT JOIN user_details ON users.id = user_details.user_id
-#     LEFT JOIN user_finance ON users.id = user_finance.user_id
-#     LEFT JOIN cars ON users.id = cars.user_id
-#     LEFT JOIN user_mobiles ON users.id = user_mobiles.user_id
-# '''
-
-# cursor.execute(query)
-
 table_name = input("Enter the name of the table you want to see: ")
 
 choice = input("select the action 'create', 'update', 'delete', 'list': ")
@@ -70,16 +41,6 @@ elif choice == "list":
 
     for row in results:
         print(row)
-
-# table_name_query = f'SELECT * FROM {table_name}'
-
-# cursor.execute(table_name_query)
-
-# rows = cursor.fetchall()
-
-
-# for row in rows:
-#     print(row)
 
 cursor.close()
 connection.close()
